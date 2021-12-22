933c2a0be3283c018648912791f9fcad94fc1b5d
axim-soft <94515531+axim-soft@users.noreply.github.com>
2021-12-22 12:37:12 +0530

    Update evaluate_model.py

--- diabetes_regression/evaluate/evaluate_model.py
+++ diabetes_regression/evaluate/evaluate_model.py
@@ -116,7 +116,10 @@ try:
         production_model_mse = 10000
         if (metric_eval in model.tags):
             production_model_mse = float(model.tags[metric_eval])
-        new_model_mse = float(run.parent.get_metrics().get(metric_eval))
+        if run.parent.get_metrics().get(metric_eval) is not None:
+            new_model_mse = float(run.parent.get_metrics().get(metric_eval))
+        else:
+            new_model_mse = None
         if (production_model_mse is None or new_model_mse is None):
             print("Unable to find", metric_eval, "metrics, "
                   "exiting evaluation")
@@ -130,14 +133,17 @@ try:
                 )
             )
 
-        if (new_model_mse < production_model_mse):
-            print("New trained model performs better, "
-                  "thus it should be registered")
+         if new_model_mse is not None:
+           if (new_model_mse < production_model_mse):
+               print("New trained model performs better, "
+                     "thus it should be registered")
+           else:
+               print("New trained model metric is worse than or equal to "
+                     "production model so skipping model registration.")
+               if((allow_run_cancel).lower() == 'true'):
+                   run.parent.cancel()
         else:
-            print("New trained model metric is worse than or equal to "
-                  "production model so skipping model registration.")
-            if((allow_run_cancel).lower() == 'true'):
-                run.parent.cancel()
+            print("New model did not perform well enough, so skipping model registration")
     else:
         print("This is the first model, "
               "thus it should be registered")
